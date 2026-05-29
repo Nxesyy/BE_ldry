dae5cc014a94c7458e2db05dfcb1c0b8e45beaec
Nxesyy <gesangrahmatullah@gmail.com>
2026-05-29 20:46:42 +0700

    fix: port: 3000

--- src/main.ts
+++ src/main.ts
@@ -34,7 +34,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  const port = process.env.PORT || 3000;
+  const port = 3000;
   await app.listen(port);
   console.log(`Application is running on: http://localhost:${port}`);
   console.log(`Swagger Docs available on: http://localhost:${port}/api/docs`);
