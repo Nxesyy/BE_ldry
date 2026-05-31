3fb6e251d596345e15c9b28d7e5a0d8007025114
Nxesyy <gesangrahmatullah@gmail.com>
2026-05-31 16:01:45 +0700

    add migration

---
+++ prisma/migrations/20260531090111_add_phone/migration.sql
@@ -0,0 +1,67 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `phone` INTEGER NULL,
+    `role` ENUM('ADMIN', 'CUSTOMER', 'ROOT') NOT NULL DEFAULT 'CUSTOMER',
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Order` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `orderCode` VARCHAR(191) NOT NULL,
+    `userId` INTEGER NOT NULL,
+    `weight` DOUBLE NOT NULL,
+    `totalPrice` INTEGER NOT NULL,
+    `status` ENUM('DIPROSES', 'DICUCI', 'DISETRIKA', 'SELESAI', 'DIAMBIL') NOT NULL DEFAULT 'DIPROSES',
+    `finishedAt` DATETIME(3) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Order_orderCode_key`(`orderCode`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `OrderHistory` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `orderId` INTEGER NOT NULL,
+    `status` ENUM('DIPROSES', 'DICUCI', 'DISETRIKA', 'SELESAI', 'DIAMBIL') NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Transaction` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `transactionCode` VARCHAR(191) NOT NULL,
+    `amount` INTEGER NOT NULL,
+    `paymentStatus` ENUM('UNPAID', 'PENDING', 'PAID', 'REJECTED') NOT NULL DEFAULT 'UNPAID',
+    `paymentMethod` VARCHAR(191) NOT NULL,
+    `paymentProof` VARCHAR(191) NULL,
+    `paidAt` DATETIME(3) NULL,
+    `orderId` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Transaction_transactionCode_key`(`transactionCode`),
+    UNIQUE INDEX `Transaction_orderId_key`(`orderId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Order` ADD CONSTRAINT `Order_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `OrderHistory` ADD CONSTRAINT `OrderHistory_orderId_fkey` FOREIGN KEY (`orderId`) REFERENCES `Order`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Transaction` ADD CONSTRAINT `Transaction_orderId_fkey` FOREIGN KEY (`orderId`) REFERENCES `Order`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
