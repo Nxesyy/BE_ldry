92eb8d6ccb586003e5bfe0e4ac74bc034e1b48dc
Nxesyy <gesangrahmatullah@gmail.com>
2026-05-31 23:52:50 +0700

    Fix error v1 Memisahkan_admin_dan_customer

---
+++ prisma/migrations/20260531165108_add_user_admin/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE `userAdmin` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `role` ENUM('ADMIN', 'CUSTOMER', 'ROOT') NOT NULL DEFAULT 'ADMIN',
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `userAdmin_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -4,7 +4,6 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   private readonly logger = new Logger(PrismaService.name);
-  userAdmin: any;
 
   constructor() {
     super({
